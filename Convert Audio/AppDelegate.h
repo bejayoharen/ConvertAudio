f5e17c59a247b864b3611b619b5f8139e2da14ea
Bjorn Roche <bjorn@xowave.com>
2015-03-17 11:53:55 -0400

    Initial Commit

---
+++ Convert Audio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Convert Audio
+//
+//  Created by Bjorn Roche on 3/17/15.
+//  Copyright (c) 2015 Bjorn Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Convert Audio/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Convert Audio
+//
+//  Created by Bjorn Roche on 3/17/15.
+//  Copyright (c) 2015 Bjorn Roche. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
